aeebc6309a033f7d39d6b91c03fecd746f706d48
Eko Puji Pramono <epujipramono@yahoo.co.id>
2026-07-07 20:32:20 +0700

    Add locator using id attribute

--- tests/learn.spec.ts
+++ tests/learn.spec.ts
@@ -19,4 +19,13 @@ test('learn pw on website formy project - autocomplete page', async ({ page }) =
   await page.getByRole('textbox', { name: 'Zip code' }).fill('16111');
   await page.getByRole('textbox', { name: 'Country' }).fill('Indonesia');
 
+/* 
+  // menggunakan id attribute
+  await page.locator('[id="autocomplete"]').fill('Kavling 234');
+  await page.locator('[id="street_number"]').fill('Jalan Raya Bogor');
+  await page.locator('[id="locality"]').fill('Bogor');
+  await page.locator('[id="administrative_area_level_1"]').fill('Jawa Barat');
+  await page.locator('[id="postal_code"]').fill('16111');
+  await page.locator('[id="country"]').fill('Indonesia');
+ */
 }); 
