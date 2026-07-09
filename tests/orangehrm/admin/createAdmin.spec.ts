214aa0ae1e414d95a1df269eaf4970806069da3d
Eko Puji Pramono <epujipramono@yahoo.co.id>
2026-07-09 09:25:19 +0700

    orangehrm: fix locators createAdmin

--- tests/orangehrm/admin/createAdmin.spec.ts
+++ tests/orangehrm/admin/createAdmin.spec.ts
@@ -16,21 +16,22 @@ test('create admin if already exists', async ({ page }) => {
   await page.getByRole('option', { name: 'Admin' }).click();
 
   // status
-  await page.locator('div:nth-child(3) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click();
+  await page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').last().click();
   await page.getByRole('option', { name: 'Enabled' }).click();
   
   // password
-  await page.getByRole('textbox').nth(3).fill('admin12345');
+  await page.locator('[type="password"]').nth(0).fill('admin12345');
   
   // employee name
   await page.getByRole('textbox', { name: 'Type for hints...' }).fill('Joseph');
   await page.getByRole('option', { name: 'Joseph Evans' }).click();
   
   // username
-  await page.getByRole('textbox').nth(2).fill('johndoee2');
+  const username = `johndoe${Math.floor(Math.random() * 10000)}`;
+  await page.getByRole('textbox').nth(2).fill(username);
 
   // confirm password
-  await page.getByRole('textbox').nth(4).fill('admin12345');
+  await page.locator('[type="password"]').nth(1).fill('admin12345');
   await page.waitForTimeout(5000);
   
   // click save
