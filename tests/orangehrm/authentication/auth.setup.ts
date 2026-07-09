8337c247e2ed10989a0dbe1660f6e73d07d21b9c
Eko Puji Pramono <epujipramono@yahoo.co.id>
2026-07-09 09:33:56 +0700

    oranghrm: create authentication folder

--- tests/orangehrm/authentication/auth.setup.ts
+++ tests/orangehrm/authentication/auth.setup.ts
@@ -1,7 +1,7 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 
-const authFile = path.join(__dirname, '../../playwright/.auth/user.json');
+const authFile = path.join(__dirname, '../../../playwright/.auth/user.json');
 
 setup('authenticate token', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
